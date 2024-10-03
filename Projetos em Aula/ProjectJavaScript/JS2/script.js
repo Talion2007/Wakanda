fe013c0a2ca8f9c38621c3b6416a2555865af866
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2024-10-03 10:49:57 -0300

    V2

--- Projetos em Aula/ProjectJavaScript/JS2/script.js
+++ Projetos em Aula/ProjectJavaScript/JS2/script.js
@@ -1,6 +1,26 @@
 document.getElementById('titulo').innerText = "Olá, Mundo em JavaScript Externo!";
 
-let saudacao = "Salve";
-let nome = "Antedigmon";
-let mensagem = saudacao + ", " + nome + "!";
-console.log(mensagem);
+//? Está isso funcionando ? Acho que sim
+let Mizu = "Kon'nichiwa​";
+let Desu = "Vitor Hugo";
+let OchaUno = Mizu + ", " + Desu + "!";
+console.log(OchaUno);
+
+//* Yamato Kudasaiiiii O~O
+// let sominha = 5 + 10
+// let resultado = "Berimbadas: " + sominha;
+// console.log(resultado);
+
+//! Top demais esses comentários coloridos
+let Gohan = "Sekkusu";
+let OchaDuo = `${Gohan}, ${Desu}? Kudasai? (O~O)`;
+console.log(OchaDuo);
+
+// Todo mundo vai pirar com essas cores
+let OchaTri = `Yamato Kudasaiiiiiiiiii... Yamato Kudasaiiiii, ${Desu}... Yamato... Kuda... SAIIIIIIIIII (>O<)`;
+console.log(OchaTri);
+
+//? Está gostando da história?
+let Sushi = "Arigatou Gozai Masu";
+let OchaQuo = `Awawwww, ${Desu}! ${Sushi}! (^O^)`;
+console.log(OchaQuo);
